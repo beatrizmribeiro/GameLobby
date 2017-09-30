1e2405d5dfc9ddc704b57b8075e4d9ee45a874f6
bcollins0905 <b.w.collinssf@gmail.com>
2017-09-30 13:03:51 -0700

    local host

---
+++ assets/new_npm/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  consumer_key:         'oBxxLbLrCGPNDXpyy6LG5SzDE',
+  consumer_secret:      'wuTApBbJDczQ0XociMWXCQkppBUwzI83EcGMVegsHGHNklullF',
+
+  access_token:         '2569694336-KdXG5FpxQJRIyki0AM3ReMSDrfvKGabIkzaWeiN',
+  access_token_secret:  'raiR2434eQUHfqUFCG8zQCxir4zUpJZYlsXvIyr3vBIox',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+  
+}
